301559aa1c566d36c2df6a61a6e1c2f8edb41227
Danny Kivi <sugarfreedanny@gmail.com>
2017-03-27 01:02:54 -0400

    Fixed login page title

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -8,7 +8,7 @@
 
 <head>
     <meta charset="utf-8" />
-    <title>Metronic Admin Theme #1 | User Login 1</title>
+    <title>Diabeasy Login</title>
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta content="width=device-width, initial-scale=1" name="viewport" />
     <meta content="Preview page of Metronic Admin Theme #1 for " name="description" />
