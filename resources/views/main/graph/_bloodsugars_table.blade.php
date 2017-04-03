cda4ecf59d388e843cec9190e8ed5028fbb0e288
Danny Kivi <sugarfreedanny@gmail.com>
2017-04-03 00:39:08 -0400

    Fixed visual bug on graph table

--- resources/views/main/graph/_bloodsugars_table.blade.php
+++ resources/views/main/graph/_bloodsugars_table.blade.php
@@ -1,8 +1,8 @@
 <table class="table table-striped table-bordered table-hover">
     <thead>
     <tr>
-        <th style="width: 10%"> Date/Time </th>
-        <th style="width: 50%"> Overview </th>
+        <th style="width: 20%"> Date/Time </th>
+        <th style="width: 40%"> Overview </th>
         <th style="width: 10%"> Notes </th>
         <th style="width: 30%"> Actions </th>
     </tr>
