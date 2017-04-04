63bb99be659cf96142c6dd4e252403007ebc8ed7
Daniel Kivi <sugarfreedanny@gmail.com>
2017-04-04 04:39:36 -0400

    Fixed logo link

--- resources/views/main/layout/header/logo.blade.php
+++ resources/views/main/layout/header/logo.blade.php
@@ -1,5 +1,5 @@
 <div class="page-logo">
-    <a href="index.html">
+    <a href="{{ route('home') }}">
         <img src="{{ asset('/logo.png') }}" alt="logo" class="logo-default" /> </a>
     <div class="menu-toggler sidebar-toggler">
         <span></span>
